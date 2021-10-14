af1bf2f8fa596cce44c9501f79a96ed8b9f36f18
Zdeněk Pašek <me@zdenekpasek.cz>
2021-10-14 19:08:29 +0200

    ClassLibrary

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public int ISBN13 { get; set; }
+
+        public Book()
+        {
+            
+        }
+
+        public Book(string title, string author, int pageNumber, int isbn13)
+        {
+            Title = title;
+            Author = author;
+            PageNumber = pageNumber;
+            ISBN13 = isbn13;
+        }
+    }
+}
